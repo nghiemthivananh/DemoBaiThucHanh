4fcc0170c475d84bff566476b7c752e566aab553
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-09-07 18:00:06 +0700

    C# ToanTu

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,18 +4,18 @@ public class Program {
     private static void Main(string[] args)
     {
 
-      const int namLamViec = 9;
-      const string HoTen = "Nghiem Thi Van Anh";
-      const int MSV = 2021050078;
-      const float a = 0.3f;
-      const double b = 4.5;
-      const decimal d = 3m;
-      const bool e = true;
-      const char KyTu = 'v';
-      Console.WriteLine("Nhanvien {0} + {1} nam kinh nghiem",HoTen,namLamViec);
-    
-      Console.WriteLine("Ma sinh vien là:{0}  ", +MSV);
-      Console.WriteLine( HoTen);
+      int a,b;
+      System.Console.Write("a = ");
+       a = Convert.ToInt32(Console.ReadLine());
+      
+      System.Console.Write("b = ");
+       b = Convert.ToInt32(Console.ReadLine());
+       
+
+        System.Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
+        System.Console.WriteLine("{0} - {1} = {2}",a,b,a-b);
+        System.Console.WriteLine("{0} / {1} = {2}",a,b,a/b);
+        System.Console.WriteLine("{0} % {1} = {2}",a,b,a%b);
  
 }
 }
