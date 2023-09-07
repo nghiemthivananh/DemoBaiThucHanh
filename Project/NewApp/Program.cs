fd261a36e00ed55f55646edb5051abb334b8d42f
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-09-07 18:20:54 +0700

    C# Break

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,14 +4,11 @@ public class Program {
     private static void Main(string[] args)
     {
 
-    int a = 1;
-    do
-    {
-      System.Console.WriteLine("Vòng lặp thứ {0}",a);
-      a++;// tăng a lên 1 đơn vị để đảm bảo có lúc a>= 10; a>=10 thì vòng lặp kết thúcthúc
-
-    }while(a <10);
-   
+   for(int i = 0; i < 10; i++)
+      {
+        if(i ==5) break;
+        System.Console.WriteLine("Vòng lặp thứ {0}",i);
+    }
    
 }
 }
