49ec02cd5f3519c3af13883aa55e9f28bb0daa60
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-09-07 18:15:53 +0700

    C# while

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,14 +4,16 @@ public class Program {
     private static void Main(string[] args)
     {
 
-    for(int i = 0; i < 10; i++)
-      {
-        System.Console.WriteLine("Vòng lặp thứ {0}",i);
-      } 
+    int a = 1;
+    while(a < 10)
+    {
+      System.Console.WriteLine("Vòng lặp thứ {0}",a);
+      a++;
    
 }
 }
 
+}
 
  
  
