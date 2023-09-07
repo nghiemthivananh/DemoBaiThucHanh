90176f3dc00db7a16cc3aed77ebca103e4e82556
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-09-07 18:22:13 +0700

    C# Continue

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -6,7 +6,7 @@ public class Program {
 
    for(int i = 0; i < 10; i++)
       {
-        if(i ==5) break;
+        if(i ==5) continue;
         System.Console.WriteLine("Vòng lặp thứ {0}",i);
     }
    
