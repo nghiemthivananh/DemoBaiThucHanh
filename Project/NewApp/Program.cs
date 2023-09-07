342739a46fba4ec94aa876cb290b6678dbaeaa53
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-09-07 18:14:12 +0700

    C# for

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -4,19 +4,10 @@ public class Program {
     private static void Main(string[] args)
     {
 
-    int day = 2;
-    switch(day){ 
-      case 1 : System.Console.WriteLine("Chu nhat"); break;
-      case 2 : System.Console.WriteLine("Thu 2"); break;
-      case 3 : System.Console.WriteLine("Thu 3"); break;
-      case 4 : System.Console.WriteLine("Thu 4"); break;
-      case 5 : System.Console.WriteLine("Thu 5"); break;
-      case 6 : System.Console.WriteLine("Thu 6"); break;
-      case 7 : System.Console.WriteLine("Thu 7"); break;
-      default :
-        System.Console.WriteLine("Khong phai ngay trong tuan");
-        break;
-    }  
+    for(int i = 0; i < 10; i++)
+      {
+        System.Console.WriteLine("Vòng lặp thứ {0}",i);
+      } 
    
 }
 }
