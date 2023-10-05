8b4c8d7a939ad8fea84ce78881a4168efae734db
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-10-05 12:24:47 +0700

    Array_Person

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,29 +1,35 @@
-﻿
-using System.Runtime.Intrinsics.Arm;
+﻿using System.Runtime.ConstrainedExecution;
+using System.Dynamic;
 using NewApp.Models;
-
-//Nghiem Thi Van Anh - MSV 2021050078
-
-public class Program
+internal class Program
 {
-  public static void Main(string[] args)
-  {
-    Student std = new Student();
-    std.EnterData();
-    std.Display();
+    private static void Main(string[] args){
+            //Nghiem Thi Van Anh- 2021050078
+    int n;
+    do {
+        try{
+            System.Console.Write("n = ");
+            n = Convert.ToInt32(Console.ReadLine());
 
+        }
+        catch {
+            n = 0;
+        }
+    }while (n<1);
 
-      }
+    Person [] A = new Person[n];
+    for(int i = 0; i < A.Length; i++ )
+    {//Nghiem Thi Van Anh- 2021050078
+         System.Console.WriteLine("A[{0}]:", i);
+         Person ps = new Person();
+         ps.EnterData();
+         A[i] = ps;
+    }
 
-  }
-
-
-
-
-    
- 
-   
-
-
- 
-
+    // HIen thi cac phan tu trong array
+    for(int i = 0; i < A.Length; i++)
+    {
+        System.Console.WriteLine(i + " FullName:  - " + A[i].FullName + " Address: - " + A[i].Address + " Age: - " + A[i].Age );
+    }
+        }
+    }
