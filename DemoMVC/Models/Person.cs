44c6a8736c67eb36a956d9836e3f304117c9f94a
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-11-09 09:58:14 +0700

    MVC_Slide 67_Person

--- DemoMVC/Models/Person.cs
+++ DemoMVC/Models/Person.cs
@@ -11,6 +11,7 @@ namespace DemoMVC.Models
         public string PersonID { get; set; }
         public string FullName { get; set; }
         public string Address { get; set; }
+        public int Age { get; set; }
     }
 }
   //Nghiem Thi Van Anh - MSV : 2021050078
