356dafd951829910322e566025f2f5f436654d80
nghiemthivananh <nghiemthivananh13112002@gmail.com>
2023-12-14 17:36:42 +0700

    phan trang

--- ss/Controllers/PersonController.cs
+++ ss/Controllers/PersonController.cs
@@ -9,6 +9,7 @@ using DemoMVC.Data;
 using DemoMVC.Models;
 using DemoMVC.Models.Process;
 using OfficeOpenXml;
+using X.PagedList;
 namespace DemoMVC.Controllers
 {
     public class PersonController : Controller
@@ -23,11 +24,22 @@ namespace DemoMVC.Controllers
         private ExcelProcess _excelPro = new ExcelProcess();
 
         // GET: Person
-        public async Task<IActionResult> Index()
+public async Task<IActionResult> Index(int? page, int? PageSize)
         {
-            return View(await _context.Person.ToListAsync());
+            ViewBag.PageSize = new List<SelectListItem>()
+            {
+                new SelectListItem() { Value="3", Text="3"},
+                new SelectListItem() { Value="5", Text="5"},
+                new SelectListItem() { Value="10", Text="10"},
+                new SelectListItem() { Value="15", Text="15"},
+                new SelectListItem() { Value="25", Text="25"},
+                new SelectListItem() { Value="50", Text="50"},
+            };
+            int pagesize = (PageSize ?? 3);
+            ViewBag.psize = pagesize;
+            var model = _context.Person.ToList().ToPagedList(page ?? 1, pagesize);
+            return View(model);
         }
-
         // GET: Person/Details/5
         public async Task<IActionResult> Details(string id)
         {
